67c273b71b32044658333e356f967fc645217e3c
GGasset <gassetgerman@gmail.com>
2025-06-09 12:26:07 +0200

    added error checking

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	size_t paco_count = 300;
 	Zombie *horde = zombieHorde(paco_count, "Los comunistas y unidos 300s Pacos");
-	for (size_t i = 0; i < paco_count; i++)
+	for (size_t i = 0; horde && i < paco_count; i++)
 	{
 		horde[i].announce();
 	}
