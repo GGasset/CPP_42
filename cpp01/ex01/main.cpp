33cfcc245425e38df1209e71a0e4a7ccd7baef77
GGasset <gassetgerman@gmail.com>
2025-06-09 12:25:01 +0200

    added ex01

---
+++ cpp01/ex01/main.cpp
@@ -0,0 +1,13 @@
+
+#include "Zombie.hpp"
+
+int main()
+{
+	size_t paco_count = 300;
+	Zombie *horde = zombieHorde(paco_count, "Los comunistas y unidos 300s Pacos");
+	for (size_t i = 0; i < paco_count; i++)
+	{
+		horde[i].announce();
+	}
+	delete[] horde;
+}
