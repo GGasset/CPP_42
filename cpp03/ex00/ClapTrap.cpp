360623a9e718d061068ee7867c066f5211a5ac88
GGasset <gassetgerman@gmail.com>
2025-07-22 17:08:18 +0200

    added attack, take damage and beRepaired definitions

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -21,9 +21,61 @@ ClapTrap::ClapTrap(const ClapTrap &src) : name(src.name), hitPoints(src.hitPoint
 ClapTrap &ClapTrap::operator=(const ClapTrap &src)
 {
 	ClapTrap out(src);
+	std::cout << "Copy assignment called!" << std::endl;
 }
 
 ClapTrap::~ClapTrap()
 {
 	std::cout << "Destructor called!" << std::endl;
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (!energyPoints)
+	{
+		std::cout << name << " tried attacking but " << name << " doesn't have energy and temporary suffocated instead ;((" << std::endl;
+		return;
+	}
+	else if (hitPoints <= 0)
+	{
+		std::cout << name << " was dead, however, his malfunctioning cpu decided he wanted world domination, the cpu burned succesfully." << std::endl;
+		return;
+	}
+
+	energyPoints--;
+	std::cout << name << " has attacked " << target << ", " << target << " lost " << attackDamage << " points!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (hitPoints <= 0)
+	{
+		std::cout << "Someone attacked " << name << " even though he was dead, gross :(" << std::endl;
+		return;
+	}
+
+	std::cout << name << " had " << hitPoints << " of health, but got attacked, causing " << amount << " damage ";
+
+	hitPoints -= amount;
+	if (hitPoints <= 0)
+		std::cout << name << " died." << std::endl;
+	else
+		std::cout << name << " is left with" << hitPoints << " health." << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (hitPoints <= 0)
+	{
+		std::cout << "Even though he was dead " << name << " tried repairing itself, he successfully broke its arm and drained its batery completely." << std::endl;
+		return;
+	}
+	std::cout << name << " tried reparing itself, having " << hitPoints;
+	if (energyPoints <= 0)
+		std::cout << " but didn't have any energy left, it soldered itself to the ground instead :((" << std::endl;
+	else
+	{
+		hitPoints += amount;
+		std::cout << " he succesfully repaired himself and was left with " << hitPoints << " health." << std::endl;
+	}
 }
