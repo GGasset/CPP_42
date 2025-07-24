d72fcc28dc820c0de19505452897ccc3a34f5c82
GGasset <gassetgerman@gmail.com>
2025-07-24 12:52:13 +0200

    added Cat class

---
+++ cpp04/ex00/Cat.cpp
@@ -0,0 +1,30 @@
+
+#include "Cat.hpp"
+
+Cat::Cat()
+{
+	std::cout << "Cat default constructor called" << std::endl;
+	type = "Cat";
+}
+
+Cat::Cat(const Cat &src)
+{
+	std::cout << "Cat copy constructor called" << std::endl;
+	*this = src;
+}
+
+Cat &Cat::operator=(const Cat &src)
+{
+	std::cout << "Cat assignment operator called" << std::endl;
+	return *this;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound()
+{
+	std::cout << "Meow" << std::endl;
+}
---
+++ cpp04/ex00/Cat.hpp
@@ -0,0 +1,19 @@
+
+#ifndef CAT_HPP
+# define CAT_HPP
+
+#include "iostream"
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+public:
+	Cat();
+	Cat(const Cat &src);
+	Cat &operator=(const Cat &src);
+	~Cat();
+
+	void makeSound();
+};
+
+#endif
