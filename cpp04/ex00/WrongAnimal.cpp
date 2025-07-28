d53fe473a690e859c0b942bc7f97a85cb8ed187c
GGasset <gassetgerman@gmail.com>
2025-07-28 12:24:56 +0200

    added wrong animal and wrong cat classes

---
+++ cpp04/ex00/WrongAnimal.cpp
@@ -0,0 +1,29 @@
+#include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal(): type("wrong animal")
+{
+	std::cout << "Wrong animal default constructor called" << std::endl;
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal &src)
+{
+	std::cout << "Wrong animal copy constructor called" << std::endl;
+	*this = src;
+}
+
+WrongAnimal &WrongAnimal::operator=(const WrongAnimal &src)
+{
+	std::cout << "Wrong animal assignment operator called" << std::endl;
+	type = src.type;
+	return *this;
+}
+
+WrongAnimal::~WrongAnimal()
+{
+	std::cout << "Wrong animal destructor called" << std::endl;
+}
+
+void WrongAnimal::makeSound()
+{
+	std::cout << "[A strange sound is heard and ignored coming from the background, it somehow feels wrong ]" << std::endl;
+}
---
+++ cpp04/ex00/WrongCat.cpp
@@ -0,0 +1,24 @@
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat()
+{
+	std::cout << "Wrong cat constructor called" << std::endl;
+	type = "Wrong cat";
+}
+
+WrongCat::WrongCat(const WrongCat &src)
+{
+	*this = src;
+	std::cout << "Wrong cat copy constructor called" << std::endl;
+}
+
+WrongCat &WrongCat::operator=(const WrongCat &src)
+{
+	type = src.type;
+	std::cout << "Wrong cat copy assignment called" << std::endl;
+}
+
+WrongCat::~WrongCat()
+{
+	std::cout << "Wrong cat destructor called" << std::endl;
+}
