0263e3013f3a438f7e97bccf32f683396a34fc33
GGasset <gassetgerman@gmail.com>
2025-07-31 13:17:04 +0200

    added character implementation

---
+++ cpp04/ex03/Character.cpp
@@ -0,0 +1,80 @@
+#include "Character.hpp"
+
+Character::Character()
+{
+	std::cout << "Character default constructor called" << std::endl;
+	std::memset(inventory, 0, sizeof(AMateria *) * inventory_size);
+	name = "Unknown";
+}
+
+Character::Character(const std::string name)
+{
+	std::cout << "Character constructor called" << std::endl;
+	*this = Character();
+	this->name = name; 
+}
+
+Character::Character(const Character &src)
+{
+	std::cout << "Character copy constructor called" << std::endl;
+	*this = src;
+}
+
+Character &Character::operator=(const Character &src)
+{
+	std::cout << "Character copy assignment called" << std::endl;
+	name = src.name;
+	for (size_t i = 0; i < inventory_size; i++)
+	{
+		delete inventory[i];
+		inventory[i] = 0;
+
+		if (src.inventory[i]) inventory[i] = src.inventory[i]->clone();
+	}
+}
+
+Character::~Character()
+{
+	std::cout << "Character destructor called" << std::endl;
+	for (size_t i = 0; i < inventory_size; i++)
+	{
+		delete inventory[i];
+	}
+}
+
+std::string const &Character::getName() const
+{
+	return name;
+}
+
+void Character::equip(AMateria *m)
+{
+	for (size_t i = 0; i < inventory_size; i++)
+	{
+		if (!inventory[i])
+		{
+			inventory[i] = m;
+			return;
+		}
+	}
+}
+
+void Character::unequip(int idx)
+{
+	if (idx < 0 || idx >= inventory_size) return;
+	inventory[idx] = 0;
+}
+
+AMateria *Character::getMateria(int idx)
+{
+	if (idx < 0 || idx >= inventory_size) return;
+	return inventory[idx];
+}
+
+void Character::use(int idx, ICharacter &target)
+{
+	if (idx < 0 || idx >= inventory_size) return;
+	if (!inventory[idx]) return;
+
+	inventory[idx]->use(target);
+}
---
+++ cpp04/ex03/Character.hpp
@@ -0,0 +1,28 @@
+
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+#include "ICharacter.hpp"
+#include "cstring"
+
+class Character : ICharacter
+{
+protected:
+	static const size_t inventory_size = 4;
+	AMateria *inventory[Character::inventory_size];
+
+public:
+	Character();
+	Character(const std::string name);
+	Character(const Character &src);
+	Character &operator=(const Character &src);
+	~Character();
+
+	std::string const & getName() const;
+	void equip(AMateria* m);
+	void unequip(int idx);
+	AMateria *getMateria(int idx);
+	void use(int idx, ICharacter& target);
+};
+
+#endif
