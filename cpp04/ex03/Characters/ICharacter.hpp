ae82a4e89e5539778527229b9d948ccf36021c91
GGasset <gassetgerman@gmail.com>
2025-08-04 13:57:41 +0200

    added unequip delete

--- cpp04/ex03/Characters/ICharacter.hpp
+++ cpp04/ex03/Characters/ICharacter.hpp
@@ -17,6 +17,8 @@ public:
 	virtual std::string const & getName() const = 0;
 	virtual void equip(AMateria* m) = 0;
 	virtual void unequip(int idx) = 0;
+	virtual void unequip_delete(int idx) = 0;
+
 	virtual AMateria *getMateria(int idx) = 0;
 	virtual void use(int idx, ICharacter& target) = 0;
 };
